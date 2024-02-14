97400f69f3131cfdfb0327c622e25dfe8650a75b
SmakleAst <SmakleLover@yandex.ru>
2024-02-14 19:22:49 +0300

    add CheckAccountType

---
+++ BankAccount.Backend/BankAccount.Domain/Enum/CheckAccountType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankAccount.Domain.Enum
+{
+    public enum CheckAccountType
+    {
+        [Display(Name = "Кредитный")]
+        Credit = 0,
+
+        [Display(Name = "Дебетовый")]
+        Debit = 1,
+    }
+}
