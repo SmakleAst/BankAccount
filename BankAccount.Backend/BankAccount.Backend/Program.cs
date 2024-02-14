2dcc878d7b93af5d7e1e90a32938725b4360804c
SmakleAst <SmakleLover@yandex.ru>
2024-02-14 19:52:48 +0300

    Configure DI

--- BankAccount.Backend/BankAccount.Backend/Program.cs
+++ BankAccount.Backend/BankAccount.Backend/Program.cs
@@ -1,3 +1,11 @@
+using BankAccount.DAL;
+using BankAccount.DAL.Interfaces;
+using BankAccount.DAL.Repositories;
+using BankAccount.Domain.Entity;
+using BankAccount.Service.Implementations;
+using BankAccount.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IBaseRepository<CheckAccountEntity>, CheckAccountRepository>();
+builder.Services.AddScoped<IBaseRepository<SaveAccountEntity>, SaveAccountRepository>();
+builder.Services.AddScoped<IBaseRepository<LegalEntity>, LegalRepository>();
+builder.Services.AddScoped<IBaseRepository<IndividualEntity>, IndividualRepository>();
+builder.Services.AddScoped<IBankService, BankService>();
+
+var connectionString = builder.Configuration.GetConnectionString("MSSQL");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
 
 var app = builder.Build();
 
