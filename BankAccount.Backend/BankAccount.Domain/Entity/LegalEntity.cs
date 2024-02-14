887f9f1eb1e2496c5a66d088229d0aeaefed7f86
SmakleAst <SmakleLover@yandex.ru>
2024-02-14 19:26:37 +0300

    Add Ownerships

--- BankAccount.Backend/BankAccount.Domain/Entity/LegalEntity.cs
+++ BankAccount.Backend/BankAccount.Domain/Entity/LegalEntity.cs
@@ -5,9 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public string LeaderFullname
+        public string LeaderFullname { get; set; }
         public string ChiefAccountantFullname { get; set; }
         public string Phone { get; set; }
-        public Ownerships OwnershipsForm { get; set; }//(государственная, частная, иностранное предприятие, смешанная)
+        public Ownerships OwnershipsForm { get; set; }
     }
 }
---
+++ BankAccount.Backend/BankAccount.Domain/Enum/Ownerships.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankAccount.Domain.Enum
+{
+    public enum Ownerships
+    {
+        [Display(Name = "Государственная")]
+        State = 0,
+
+        [Display(Name = "Частная")]
+        PrivateForm = 1,
+
+        [Display(Name = "Иностранное предприятие")]
+        ForeignEnterprise = 2,
+
+        [Display(Name = "Cмешанная")]
+        Mixed = 3, 
+    }
+}
