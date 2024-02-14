d0d268efc0363fdad17196262fad0991f2b3866a
SmakleAst <SmakleLover@yandex.ru>
2024-02-14 19:27:48 +0300

    Add Gender enum

---
+++ BankAccount.Backend/BankAccount.Domain/Enum/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankAccount.Domain.Enum
+{
+    public enum Gender
+    {
+        [Display(Name = "Мужчина")]
+        Male = 0,
+
+        [Display(Name = "Женщина")]
+        Female = 1,
+    }
+}
