f4119162e01ad95586bfe8976ed9bd01c7cfdf89
SmakleAst <SmakleLover@yandex.ru>
2024-03-13 00:23:37 +0300

    Add rest for account and legal client into IBankService

--- BankAccount.Backend/BankAccount.Service/Interfaces/IBankService.cs
+++ BankAccount.Backend/BankAccount.Service/Interfaces/IBankService.cs
@@ -1,6 +1,21 @@
-﻿namespace BankAccount.Service.Interfaces
+﻿using BankAccount.Domain.Entity;
+using BankAccount.Domain.Response;
+using BankAccount.Domain.ViewModels;
+
+namespace BankAccount.Service.Interfaces
 {
     public interface IBankService
     {
+        Task<IBaseResponse<CreateAccountViewModel>> CreateAccount(CreateAccountViewModel model);
+        Task<IBaseResponse<AccountEntity>> DeleteAccount(int id);
+        Task<IBaseResponse<AccountEntity>> UpdateAccount(UpdateAccountViewModel model);
+        Task<IBaseResponse<AccountEntity>> PatchAccount(UpdateAccountViewModel model);
+        Task<IBaseResponse<IEnumerable<AccountEntity>>> GetAllAccounts();
+
+        Task<IBaseResponse<CreateLegalClientViewModel>> CreateLegalClient(CreateLegalClientViewModel model);
+        Task<IBaseResponse<LegalClientEntity>> DeleteLegalClient(int id);
+        Task<IBaseResponse<LegalClientEntity>> UpdateLegalClient(UpdateLegalClientViewModel model);
+        Task<IBaseResponse<LegalClientEntity>> PatchLegalClient(UpdateLegalClientViewModel model);
+        Task<IBaseResponse<IEnumerable<LegalClientEntity>>> GetAllLegalClients();
     }
 }
